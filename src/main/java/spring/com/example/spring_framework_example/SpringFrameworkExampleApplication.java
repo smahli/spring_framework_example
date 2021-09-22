d23f9d24f8f783c55ff47942bc9923f20fe004db
smahli <mahlishiwshankar@gmail.com>
2021-09-21 23:29:56 -0700

    Initial commit

---
+++ src/main/java/spring/com/example/spring_framework_example/SpringFrameworkExampleApplication.java
@@ -0,0 +1,13 @@
+package spring.com.example.spring_framework_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFrameworkExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/spring/com/example/spring_framework_example/SpringFrameworkExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.com.example.spring_framework_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
